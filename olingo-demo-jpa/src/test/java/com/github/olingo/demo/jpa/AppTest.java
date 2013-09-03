00142cf8caeca3333f74d2dde07b5129fb6214a5
moscac <moscac@156.123.208.61>
2013-09-03 16:31:39 -0400

    modified for test data

--- olingo-demo-jpa/src/test/java/com/github/olingo/demo/jpa/AppTest.java
+++ olingo-demo-jpa/src/test/java/com/github/olingo/demo/jpa/AppTest.java
@@ -1,38 +1,54 @@
 package com.github.olingo.demo.jpa;
 
-import junit.framework.Test;
+import java.io.InputStream;
+import java.sql.Connection;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/application-context.xml"})
+public class AppTest extends TestCase {
+
+    private static final String FLAT_XML_DATASET = "FlatXmlDataSet.xml";
+    @Autowired
+    BasicDataSource bds;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        DatabaseOperation.CLEAN_INSERT.execute(getConnection(), getDataSet());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testApp() {
+        assertTrue(true);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    private IDataSet getDataSet() throws Exception {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(FLAT_XML_DATASET);
+        FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+        IDataSet dataset = builder.build(inputStream);
+        return dataset;
+    }
+
+    private IDatabaseConnection getConnection() throws Exception {
+
+        Connection jdbcConnection = bds.getConnection();
+        IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+        return connection;
     }
 }
